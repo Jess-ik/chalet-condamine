d5f41ac0ae22eb35dc6d881b4ac91ae69f689ac2
Jess <jessica.louvel@gmail.com>
2024-02-01 09:52:35 +0100

    Responsive Landing

--- src/slices/Landing/index.tsx
+++ src/slices/Landing/index.tsx
@@ -5,8 +5,8 @@ import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismic
 
 // Rich text styling
 const components: JSXMapSerializer = {
-	heading1: ({ children }) => <h1 className="max-w-2xl  font-heading text-8xl leading-[6rem] font-light">{children}</h1>,
-	paragraph: ({ children }) => <p className="text-3xl font-extralight  ">{children}</p>,
+	heading1: ({ children }) => <h1 className="max-w-xl lg:max-w-2xl font-heading text-7xl md:text-8xl md:leading-[6rem] font-light">{children}</h1>,
+	paragraph: ({ children }) => <p className="text-2xl md:text-3xl font-extralight ">{children}</p>,
 };
 
 // Props for `Landing`.
@@ -15,17 +15,18 @@ export type LandingProps = SliceComponentProps<Content.LandingSlice>;
 //Component for "Landing" Slices.
 const Landing = ({ slice }: LandingProps): JSX.Element => {
 	return (
-		<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="h-screen ">
-			<div className="bg-[#1F222E] grid grid-cols-3 justify-between text-white">
-				<div className="col-span-2 flex flex-col justify-center px-24 gap-8 max-w-4xl mx-auto">
+		<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="lg:h-screen">
+			<div className="bg-[#1F222E] h-screen grid grid-rows-3 lg:grid-cols-3 justify-between text-white">
+				<div className="row-span-2 lg:col-span-2 flex flex-col justify-center gap-8 px-12   max-w-4xl mx-auto">
 					<PrismicRichText field={slice.primary.heading} components={components} />
 					<PrismicRichText field={slice.primary.intro} components={components} />
 					<Button field={slice.primary.button_link} className="mt-6 bg-white">
 						{slice.primary.button_text}
 					</Button>
 				</div>
-
-				<PrismicNextImage field={slice.primary.image} className="h-screen object-cover" />
+				<div className="bg-white row-span-1 lg:h-screen">
+					<PrismicNextImage field={slice.primary.image} className="h-full object-cover" />
+				</div>
 			</div>
 		</section>
 	);
