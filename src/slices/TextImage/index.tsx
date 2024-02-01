6d3d1a936438edb064adeb81e2f6741462f950ba
Jess <jessica.louvel@gmail.com>
2024-02-01 10:08:22 +0100

    Responsive TextImage

--- src/slices/TextImage/index.tsx
+++ src/slices/TextImage/index.tsx
@@ -5,7 +5,7 @@ import clsx from "clsx";
 
 // Rich text styling
 const components: JSXMapSerializer = {
-	paragraph: ({ children }) => <p className="max-w-2xl mx-auto py-8 text-3xl font-extralight text-center ">{children}</p>,
+	paragraph: ({ children }) => <p className="max-w-2xl mx-auto py-8 text-xl md:text-2xl lg:text-3xl font-extralight text-center ">{children}</p>,
 };
 
 /**
@@ -18,9 +18,9 @@ export type TextImageProps = SliceComponentProps<Content.TextImageSlice>;
  */
 const TextImage = ({ slice }: TextImageProps): JSX.Element => {
 	return (
-		<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="px-20 py-24">
-			<div className="grid grid-cols-2 gap-20 items-center">
-      <PrismicNextImage field={slice.primary.image} className={clsx(slice.variation === "imageRight" && "order-2")}/>
+		<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="px-20 py-16 lg:py-24">
+			<div className="grid lg:grid-cols-2 gap-20 items-center">
+      <PrismicNextImage field={slice.primary.image} className={clsx(slice.variation === "imageRight" && "lg:order-2")}/>
 				<PrismicRichText field={slice.primary.body} components={components} />
       </div>     
 		</section>
