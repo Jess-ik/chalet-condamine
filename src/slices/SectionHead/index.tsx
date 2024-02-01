64bbff0778443e74796e0476929873302929ba5a
Jess <jessica.louvel@gmail.com>
2024-02-01 10:05:27 +0100

    Responsive Section Head

--- src/slices/SectionHead/index.tsx
+++ src/slices/SectionHead/index.tsx
@@ -3,7 +3,7 @@ import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismic
 
 // Rich text styling
 const components: JSXMapSerializer = {
-	heading2: ({ children }) => <h2 className=" max-w-5xl mx-auto font-heading text-8xl leading-[6rem] font-light">{children}</h2>,
+	heading2: ({ children }) => <h2 className="max-w-3xl lg:max-w-4xl mx-auto font-heading text-7xl lg:text-8xl leading-[6rem] lg:leading-[7rem] font-light">{children}</h2>,
 };
 
 /**
@@ -16,7 +16,8 @@ export type SectionHeadProps = SliceComponentProps<Content.SectionHeadSlice>;
  */
 const SectionHead = ({ slice }: SectionHeadProps): JSX.Element => {
 	return (
-		<section id={slice.primary.anchor_id || undefined} data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="scroll-section pt-52 pb-24 text-center rounded-t-[50px]">
+		<section id={slice.primary.anchor_id || undefined} data-slice-type={slice.slice_type} data-slice-variation={slice.variation}
+			className="scroll-section pt-44 lg:pt-52 pb-24 text-center rounded-t-[50px]">
 			<PrismicRichText field={slice.primary.heading} components={components}/>
 		</section>
 	);
