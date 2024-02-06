571b9f8bde62f4a8aa1df0c9a449d66167ea9d38
Jess <jessica.louvel@gmail.com>
2024-02-06 12:05:23 +0100

    fix build

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -106,7 +106,7 @@ export default function Footer() {
 				</div>
 
 				<div className="col-span-2 bg-slate-300">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d11316.143687065312!2d6.4589136!3d44.8412018!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDTCsDUwJzI5LjAiTiA2wrAyNyc1NS45IkU!5e0!3m2!1sen!2sfr!4v1706698081040!5m2!1sen!2sfr" width="100%" height="100%" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d11316.143687065312!2d6.4589136!3d44.8412018!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDTCsDUwJzI5LjAiTiA2wrAyNyc1NS45IkU!5e0!3m2!1sen!2sfr!4v1706698081040!5m2!1sen!2sfr" width="100%" height="100%" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
 				</div>
 			</div>
 
