2c47fb08320b09f2fe387d6b246a55609686d499
Jess <jessica.louvel@gmail.com>
2024-02-06 11:29:44 +0100

    Anim CtA

--- src/slices/CtA/index.tsx
+++ src/slices/CtA/index.tsx
@@ -1,13 +1,22 @@
+'use client'
 import Button from "@/components/Button";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 // Rich text styling
 const components: JSXMapSerializer = {
 	heading2: ({ children }) => <h2 className="max-w-2xl uppercase text-base tracking-widest  font-light">{children}</h2>,
 	heading3: ({ children }) => <h3 className="max-w-xl lg:max-w-2xl font-heading text-7xl md:text-8xl md:leading-[6rem] font-light">{children}</h3>,
-	paragraph: ({ children }) => <p className="text-xl md:text-2xl font-thin md:font-extralight leading-[2rem]">{children}</p>,
+	paragraph: ({ text }) => (
+		<p className="text-xl md:text-2xl font-thin md:font-extralight leading-[2rem]">
+			{text.split(" ").map((word: string, index: number) => (
+				<motion.span key={index}>{word} </motion.span>
+			))}
+		</p>
+	),
 };
 
 // Props for `CtA`.
@@ -15,6 +24,31 @@ export type CtAProps = SliceComponentProps<Content.CtASlice>;
 
 //Component for "CtA" Slices.
 const CtA = ({ slice }: CtAProps): JSX.Element => {
+	const heading2 = useRef(null);
+	const heading3 = useRef(null);
+	const body = useRef(null);
+	const button = useRef(null);
+
+	const { scrollYProgress: scrollForH2 } = useScroll({
+		target: heading2,
+		offset: ["start 0.8", "start 0.3"],
+	});
+	const { scrollYProgress: scrollForH3 } = useScroll({
+		target: heading3,
+		offset: ["start 0.8", "start 0.4"],
+	});
+
+	const { scrollYProgress: scrollForBody } = useScroll({
+		target: body,
+		offset: ["start 0.8", "start 0.4"],
+	});
+
+	const { scrollYProgress: scrollForButton } = useScroll({
+		target: button,
+		offset: ["start end", "start 0.8"],
+	});
+	const scaleButtonProgress = useTransform(scrollForButton, [0, 1], [0.7, 1]);
+	const scaleH3Progress = useTransform(scrollForH3, [0, 1], [0.7, 1]);
 	return (
 		<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="h-screen">
 			<div className="bg-[#1F222E] h-screen grid grid-rows-3 lg:grid-cols-3 justify-between text-white ">
@@ -22,16 +56,24 @@ const CtA = ({ slice }: CtAProps): JSX.Element => {
 					<PrismicNextImage field={slice.primary.image} className="h-full lg:h-screen object-cover" />
 				</div>
 				<div className="row-span-2 lg:row-span-3 lg:col-span-2 flex flex-col justify-center px-24 gap-8 max-w-4xl mx-auto">
-					<PrismicRichText field={slice.primary.subhead} components={components} />
-					<PrismicRichText field={slice.primary.heading} components={components} />
-					<PrismicRichText field={slice.primary.body} components={components} />
+					<motion.div ref={heading2} style={{ opacity: scrollForH2 }}>
+						<PrismicRichText field={slice.primary.subhead} components={components} />
+					</motion.div>
+					<motion.div ref={heading3} style={{ opacity: scrollForH3 }}>
+						<PrismicRichText field={slice.primary.heading} components={components} />
+					</motion.div>
+					<motion.div ref={body} style={{ opacity: scrollForBody }}>
+						<PrismicRichText field={slice.primary.body} components={components} />
+					</motion.div>
 
 					<div className="flex gap-8 flex-col md:flex-row">
 						{/*  CTA buttons */}
 						{slice.items.map(({ button_link, button_text }) => (
-							<Button field={button_link} key={button_text} className="bg-white mt-6">
-								{button_text}
-							</Button>
+							<motion.div ref={button} style={{ opacity: scrollForButton, scale: scaleButtonProgress }}>
+								<Button field={button_link} key={button_text} className="bg-white mt-6">
+									{button_text}
+								</Button>
+							</motion.div>
 						))}
 					</div>
 				</div>
